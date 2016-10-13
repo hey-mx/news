638261f0fdde80e3c7fb006dfe64c9c6a081ea7d
Juan Carlos Juárez <isc.jcjl@me.com>
2016-10-13 01:00:54 -0500

    Agregando formulario de registro de usuarios Y almacenando los datos en Base de datos

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Form\UserType;
+use AppBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
@@ -13,10 +16,23 @@ class UserController extends Controller
         ));
     }
 
-    public function signInAction()
+    public function signInAction(Request $request)
     {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $user->setStatus(1);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            $this->redirectToRoute('home');
+        }
         return $this->render('AppBundle:User:sign_in.html.twig', array(
-            // ...
+            'form' => $form->createView(),
         ));
     }
 
